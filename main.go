c0870b40de6ed2dba7629ac973e8d0effdd0f8f1
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-02-15 07:53:49 +0200

    Implemented rotate function, but it doesn't work

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type vector struct {
+	x, y int
+}
+
+type line struct {
+	a, b vector
+}
+
+func (l *line) draw(screen *ebiten.Image) {
+	DrawLine(screen, l.a.x, l.a.y, l.b.x, l.b.y, color.RGBA{1, 100, 100, 255})
+}
+
+func rotate(x, y int, rad float64) (x1, y1 int) {
+	x1 = int(float64(x)*math.Cos(rad) - float64(y)*math.Sin(rad))
+	y1 = int(float64(y)*math.Cos(rad) - float64(x)*math.Sin(rad))
+	return x1, y1
+}
+
+const (
+	screenWidth  = 1920
+	screenHeight = 1080
+)
+
+type game struct {
+	l            line
+	screenBuffer *ebiten.Image
+}
+
+func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
+func (g *game) Update() error {
+	g.l.b.x, g.l.b.y = rotate(g.l.b.x, g.l.b.y, math.Pi/180)
+	return nil
+}
+func (g *game) Draw(screen *ebiten.Image) {
+	g.l.draw(screen)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	g := &game{}
+	g.screenBuffer = ebiten.NewImage(screenWidth, screenHeight)
+	g.l = line{vector{screenWidth / 2, screenHeight / 2}, vector{screenWidth / 4 * 3, screenHeight / 4}}
+
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
+	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
+	if abs(y2-y1) < abs(x2-x1) {
+		if x1 > x2 {
+			x1, x2 = x2, x1
+			y1, y2 = y2, y1
+		}
+		dx, dy := x2-x1, y2-y1
+		dirY := 1
+		// Dy < 0 => y2 - y1 < 0 => y1 > y2 => Growing downwards
+		if dy < 0 {
+			dirY = -1
+			dy = -dy // For us to pretend that line is growing upwards
+		}
+		d := 2*dy - dx
+		for x, y := x1, y1; x < x2; x++ {
+			img.Set(x, y, c)
+			if d >= 0 { // NE
+				y += dirY
+				d += dy - dx
+			} else { // E
+				d += dy
+			}
+		}
+	} else {
+		if y1 > y2 {
+			x1, x2 = x2, x1
+			y1, y2 = y2, y1
+		}
+		dx, dy := x2-x1, y2-y1
+		dirX := 1
+		if dx < 0 {
+			dirX = -1
+			dx = -dx
+		}
+		d := 2*dx - dy
+		for x, y := x1, y1; y < y2; y++ {
+			img.Set(x, y, c)
+			if d >= 0 { // NE
+				x += dirX
+				d += dx - dy
+			} else { // E
+				d += dx
+			}
+		}
+	}
+}
