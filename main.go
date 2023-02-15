a84ce2613da34ffab8a2f0469aa5d41207397f6e
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-02-15 15:06:29 +0200

    Got rid of rounding

--- main.go
+++ main.go
@@ -8,29 +8,25 @@ import (
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type vector struct {
-	x, y int
+	x, y float64
 }
 
 type line struct {
 	a, b vector
 }
 
-func (l *line) draw(screen *ebiten.Image) {
-	DrawLine(screen, l.a.x, l.a.y, l.b.x, l.b.y, color.RGBA{1, 100, 100, 255})
-}
-
 func (l *line) rotate(rad float64) {
-	x1, y1 := l.b.x-l.a.x, l.b.y-l.a.y
-	x1, y1 = rotate(float64(x1), float64(y1), rad)
+	x1, y1 := rotate(l.b.x-l.a.x, l.b.y-l.a.y, rad)
 	l.b = vector{x1 + l.a.x, y1 + l.a.y}
 }
 
-func rotate(x, y, rad float64) (x1, y1 int) {
-	x1 = int(x*math.Cos(rad) - y*math.Sin(rad))
-	y1 = int(y*math.Cos(rad) + x*math.Sin(rad))
+func rotate(x, y, rad float64) (x1, y1 float64) {
+	x1 = x*math.Cos(rad) - y*math.Sin(rad)
+	y1 = y*math.Cos(rad) + x*math.Sin(rad)
 	return x1, y1
 }
 
@@ -40,8 +36,7 @@ const (
 )
 
 type game struct {
-	l            line
-	screenBuffer *ebiten.Image
+	l line
 }
 
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
@@ -50,71 +45,16 @@ func (g *game) Update() error {
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
-	g.l.draw(screen)
+	ebitenutil.DrawLine(screen, g.l.a.x, g.l.a.y, g.l.b.x, g.l.b.y, color.RGBA{200, 250, 200, 250})
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	g := &game{}
-	g.screenBuffer = ebiten.NewImage(screenWidth, screenHeight)
-	g.l = line{vector{screenWidth / 2, screenHeight / 2}, vector{screenWidth / 4 * 3, screenHeight / 4}}
+	g.l = line{vector{screenWidth / 2, screenHeight / 2}, vector{screenWidth / 4 * 2, screenHeight / 4}}
 
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
-	}
-}
-
-func abs(a int) int {
-	if a >= 0 {
-		return a
-	}
-	return -a
-}
-func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
-	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
-	if abs(y2-y1) < abs(x2-x1) {
-		if x1 > x2 {
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
-		}
-		dx, dy := x2-x1, y2-y1
-		dirY := 1
-		// Dy < 0 => y2 - y1 < 0 => y1 > y2 => Growing downwards
-		if dy < 0 {
-			dirY = -1
-			dy = -dy // For us to pretend that line is growing upwards
-		}
-		d := 2*dy - dx
-		for x, y := x1, y1; x < x2; x++ {
-			img.Set(x, y, c)
-			if d >= 0 { // NE
-				y += dirY
-				d += dy - dx
-			} else { // E
-				d += dy
-			}
-		}
-	} else {
-		if y1 > y2 {
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
-		}
-		dx, dy := x2-x1, y2-y1
-		dirX := 1
-		if dx < 0 {
-			dirX = -1
-			dx = -dx
-		}
-		d := 2*dx - dy
-		for x, y := x1, y1; y < y2; y++ {
-			img.Set(x, y, c)
-			if d >= 0 { // NE
-				x += dirX
-				d += dx - dy
-			} else { // E
-				d += dx
-			}
-		}
 	}
 }
