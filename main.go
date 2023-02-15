9441c978bc3d99c5faa17de979767dfd8c014042
Vladimir Satinov <vovaumni@gmail.com>
2023-02-15 17:54:24 +0200

    rotating flag +watch

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"time"
-	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -16,42 +16,32 @@ const (
 	screenHeight = 640
 )
 
-type Point struct{
+type Point struct {
 	x, y float64
 }
 
-type Game struct{
-	width, height int
-	p1, p2 Point
+type Game struct {
+	width, height      int
+	p1, p2, p3, p4, p5 Point
 }
 
-func DrawLineDDA(img *ebiten.Image, x1, y1, x2, y2 float64, c color.Color) {
-	if math.Abs(x2-x1) <= math.Abs(y2-y1) {
-		if y2 < y1 {
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
-		}
-		k := float64(x2-x1) / float64(y2-y1)
-		for x, y := float64(x1)+0.5, y1; y <= y2; x, y = x+k, y+1 {
-			img.Set(int(x), int(y), c)
-		}
-	} else {
-		if x2 < x1 {
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
-		}
-		k := float64(y2-y1) / float64(x2-x1)
-		for x, y := x1, float64(y1)+0.5; x <= x2; x, y = x+1, y+k {
-			img.Set(int(x), int(y), c)
-		}
-	}
-}
-
-func (g *Game) Rotate(angle float64){
-	g.p1.x = g.p1.x*math.Cos(angle) - g.p1.y*math.Sin(angle)
-	g.p1.y = g.p1.x*math.Sin(angle) + g.p1.y*math.Cos(angle)
-	g.p2.x = g.p2.x*math.Cos(angle) - g.p2.y*math.Sin(angle)
-	g.p2.y = g.p2.x*math.Sin(angle) + g.p2.y*math.Cos(angle)
+func (g *Game) Rotate(angle float64) {
+	var c Point
+	c.x = g.p1.x*math.Cos(angle) - g.p1.y*math.Sin(angle)
+	c.y = g.p1.x*math.Sin(angle) + g.p1.y*math.Cos(angle)
+	g.p1.x, g.p1.y = c.x, c.y
+	c.x = g.p2.x*math.Cos(angle) - g.p2.y*math.Sin(angle)
+	c.y = g.p2.x*math.Sin(angle) + g.p2.y*math.Cos(angle)
+	g.p2.x, g.p2.y = c.x, c.y
+	c.x = g.p3.x*math.Cos(angle) - g.p3.y*math.Sin(angle)
+	c.y = g.p3.x*math.Sin(angle) + g.p3.y*math.Cos(angle)
+	g.p3.x, g.p3.y = c.x, c.y
+	c.x = g.p4.x*math.Cos(angle) - g.p4.y*math.Sin(angle)
+	c.y = g.p4.x*math.Sin(angle) + g.p4.y*math.Cos(angle)
+	g.p4.x, g.p4.y = c.x, c.y
+	c.x = g.p5.x*math.Cos(angle) - g.p5.y*math.Sin(angle)
+	c.y = g.p5.x*math.Sin(angle) + g.p5.y*math.Cos(angle)
+	g.p5.x, g.p5.y = c.x, c.y
 }
 
 func (g *Game) Layout(outWidth, outHeight int) (w, h int) {
@@ -59,22 +49,26 @@ func (g *Game) Layout(outWidth, outHeight int) (w, h int) {
 }
 
 func (g *Game) Update() error {
-	g.Rotate(math.Pi /100)
+	g.Rotate(math.Pi / 180)
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DrawCircle(screen, float64(g.width/2), float64(g.height/2), 100, color.RGBA{255, 0, 0, 50})
-	DrawLineDDA(screen, g.p1.x+float64(g.width/2), g.p1.y+float64(g.height/2), g.p2.x+float64(g.width/2), g.p2.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
-	// ebitenutil.DrawLine(screen, g.p1.x+float64(g.width/2), g.p1.y+float64(g.height/2), g.p2.x+float64(g.width/2), g.p2.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
+	ebitenutil.DrawLine(screen, g.p1.x+float64(g.width/2), g.p1.y+float64(g.height/2), g.p2.x+float64(g.width/2), g.p2.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
+	ebitenutil.DrawLine(screen, g.p2.x+float64(g.width/2), g.p2.y+float64(g.height/2), g.p3.x+float64(g.width/2), g.p3.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
+	ebitenutil.DrawLine(screen, g.p3.x+float64(g.width/2), g.p3.y+float64(g.height/2), g.p4.x+float64(g.width/2), g.p4.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
+	ebitenutil.DrawLine(screen, g.p4.x+float64(g.width/2), g.p4.y+float64(g.height/2), g.p5.x+float64(g.width/2), g.p5.y+float64(g.height/2), color.RGBA{255, 255, 255, 255})
 }
 
 func NewGame(width, height int) *Game {
 	return &Game{
 		width:  width,
 		height: height,
-		p1: Point{x:0, y:0},
-		p2: Point{x:100, y:100},
+		p1:     Point{x: 0, y: 0},
+		p2:     Point{x: 0, y: 200},
+		p3:     Point{x: -75, y: 200},
+		p4:     Point{x: -75, y: 160},
+		p5:     Point{x: 0, y: 160},
 	}
 }
 
