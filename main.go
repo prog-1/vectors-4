ae86164c011459c61fdec668217b336f068df7cd
Andrej Ceremnih <andrejceremnih@gmail.com>
2023-02-19 21:25:24 +0200

    flag rotation is done

--- main.go
+++ main.go
@@ -19,43 +19,10 @@ type Point struct {
 	x, y float64
 }
 
-type Line struct {
-	pos1, pos2 Point
-	color      color.RGBA
-}
-
-// NewLine initializes and returns a new Line instance.
-func NewLine(x1, y1, x2, y2 float64) *Line {
-	return &Line{
-		pos1: Point{x: x1, y: y1},
-		pos2: Point{x: x2, y: y2},
-		color: color.RGBA{
-			R: 0xff,
-			G: 0xff,
-			B: 0xff,
-			A: 0xff,
-		},
-	}
-}
-
-func Draw(img *ebiten.Image, x1, y1, x2, y2 float64, c color.Color) {
-	ebitenutil.DrawLine(img, x1, y1, x2, y2, c)
-}
-
-func (l *Line) Update() {
-	newx := l.pos2.x*math.Cos(math.Pi/180) - l.pos2.y*math.Sin(math.Pi/180)
-	newy := l.pos2.x*math.Sin(math.Pi/180) + l.pos2.y*math.Cos(math.Pi/180)
-	l.pos2.x, l.pos2.y = newx, newy
-}
-
-func (l *Line) Draw(screen *ebiten.Image) {
-	Draw(screen, l.pos1.x, l.pos1.y, l.pos2.x, l.pos2.y, l.color)
-}
-
 // Game is a game instance.
 type Game struct {
-	width, height int
-	line          *Line
+	width, height      int
+	p1, p2, p3, p4, p5 Point
 }
 
 // NewGame returns a new Game instance.
@@ -63,7 +30,11 @@ func NewGame(width, height int) *Game {
 	return &Game{
 		width:  width,
 		height: height,
-		line:   NewLine(float64(width/2), float64(height/2), 430, 130),
+		p1:     Point{float64(width) / 2, float64(height) / 2},
+		p2:     Point{430, 130},
+		p3:     Point{490, 180},
+		p4:     Point{460, 210},
+		p5:     Point{400, 160},
 	}
 }
 
@@ -71,15 +42,28 @@ func (g *Game) Layout(outWidth, outHeight int) (w, h int) {
 	return g.width, g.height
 }
 
+func Rotate(p1 Point, p2 *Point) {
+	var newpos Point
+	newpos.x = (p2.x-p1.x)*math.Cos(math.Pi/180) - (p2.y-p1.y)*math.Sin(math.Pi/180)
+	newpos.y = (p2.x-p1.x)*math.Sin(math.Pi/180) + (p2.y-p1.y)*math.Cos(math.Pi/180)
+	p2.x, p2.y = newpos.x+p1.x, newpos.y+p1.y
+}
+
 // Update updates a game state.
 func (g *Game) Update() error {
-	g.line.Update()
+	Rotate(g.p1, &g.p2)
+	Rotate(g.p1, &g.p3)
+	Rotate(g.p1, &g.p4)
+	Rotate(g.p1, &g.p5)
 	return nil
 }
 
 // Draw renders a game screen.
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.line.Draw(screen)
+	ebitenutil.DrawLine(screen, g.p1.x, g.p1.y, g.p2.x, g.p2.y, color.White)
+	ebitenutil.DrawLine(screen, g.p2.x, g.p2.y, g.p3.x, g.p3.y, color.White)
+	ebitenutil.DrawLine(screen, g.p3.x, g.p3.y, g.p4.x, g.p4.y, color.White)
+	ebitenutil.DrawLine(screen, g.p4.x, g.p4.y, g.p5.x, g.p5.y, color.White)
 }
 
 func main() {
